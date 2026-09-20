926f31e4e45a8337d13d3cc29561109245392cd2
OverbearingPearl <OverbearingPearl@outlook.com>
2026-09-21 01:57:54 +0800

    Bump version to 1.0.0

--- testcover-audit.el
+++ testcover-audit.el
@@ -4,7 +4,7 @@
 ;; Author: OverbearingPearl <OverbearingPearl@outlook.com>
 ;; Assisted-by: DeepSeek:deepseek-v4-flash, GLM:glm-5.3-flash, Laguna:laguna-s-2.1
 ;; URL: https://github.com/OverbearingPearl/testcover-audit
-;; Version: 0.1.12
+;; Version: 1.0.0
 ;; Package-Requires: ((emacs "27.1") (project "0.9.8"))
 ;; Keywords: tools
 ;; SPDX-License-Identifier: GPL-3.0-or-later
